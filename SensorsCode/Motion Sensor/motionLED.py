eee3742cdf422bf91615c10bac42c32bfd22bd5b
talha <talhajameel15@gmail.com>
2021-07-11 03:47:37 +0500

    Added callback func

--- SensorsCode/Motion Sensor/motionLED.py
+++ SensorsCode/Motion Sensor/motionLED.py
@@ -3,11 +3,19 @@ import time                 # importing time library for delay
 from gpiozero import MotionSensor, LED
 from signal import pause
 
+def on_motion_detected():
+    print("Motion Detected!")
+    led.on()
+
+def on_no_motion():
+    print("No Motion!")
+    led.off()
+
 pir = MotionSensor(4)    #pin number for motion sensor input
 led = LED(16)            #pin number for LED output
 
-pir.when_motion = led.on
-pir.when_no_motion = led.off
+pir.when_motion = on_motion_detected
+pir.when_no_motion = on_no_motion
 
 pause()
 
